6ca48f82274a3ca6d82a905e8f6323e4066e6348
madu33 <hashanteespring@gmail.com>
2022-07-28 10:08:23 +0530

    created start button complete

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+const screen = Dimensions.get('window');
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My Name is Hashan</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light-content" />
+      <TouchableOpacity onPress={()=> null} style={styles.button}>
+        <Text style={styles.buttonText}>Start</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +16,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    borderRadius:100,
+    borderWidth:10,
+    borderColor:'#A52A2A',
+    width:screen.width/2,
+    height:screen.width/2,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  buttonText:{
+    fontSize:45,
+    fontWeight:'bold',
+    color: '#800000',
+  }
 });
