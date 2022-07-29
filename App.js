f1a684255af5352c7f1750ed939f27ba6226ccc9
madu33 <hashanteespring@gmail.com>
2022-07-29 10:56:39 +0530

    complete project

--- App.js
+++ App.js
@@ -1,15 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{useState,useEffect} from 'react';
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions } from 'react-native';
+import {AdMobBanner} from 'expo-ads-admob';
+
 
 const screen = Dimensions.get('window');
+
+const formatNumber = number => `0${number}`.slice(-2);
+
+const getRemaining = (time) => {
+    const mins = Math.floor(time / 60);
+    const secs = time - mins * 60;
+    return { mins: formatNumber(mins), secs: formatNumber(secs) };
+}
+
 export default function App() {
+  const [remainingSecs, setRemainingSecs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const { mins, secs } = getRemaining(remainingSecs);
+
+  toggle = () => {
+    setIsActive(!isActive);
+  }
+
+  reset = () => {
+    setRemainingSecs(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setRemainingSecs(remainingSecs => remainingSecs + 1);
+      }, 1000);
+    } else if (!isActive && remainingSecs !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, remainingSecs]);
+
+
   return (
     <View style={styles.container}>
-      <StatusBar style="light-content" />
-      <TouchableOpacity onPress={()=> null} style={styles.button}>
-        <Text style={styles.buttonText}>Start</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
+      
+      <TouchableOpacity onPress={this.toggle} style={styles.button}>
+          <Text style={styles.buttonText}>{isActive ? 'Pause' : 'Start'}</Text>
       </TouchableOpacity>
+      <TouchableOpacity onPress={this.reset} style={[styles.button, styles.buttonReset]}>
+          <Text style={[styles.buttonText, styles.buttonTextReset]}>{'Reset'}</Text>
+      </TouchableOpacity>
+      
+<AdMobBanner
+  bannerSize="fullBanner"
+  adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+  servePersonalizedAds // true or false
+  onDidFailToReceiveAdWithError={this.bannerError} />
     </View>
   );
 }
@@ -17,22 +64,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#07121B',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button:{
-    borderRadius:100,
-    borderWidth:12,
-    borderColor:'#DAF7A6',
-    width:screen.width/2,
-    height:screen.width/2,
-    alignItems:'center',
-    justifyContent:'center'
+  button: {
+      borderWidth: 10,
+      borderColor: '#B9AAFF',
+      width: screen.width / 2,
+      height: screen.width / 2,
+      borderRadius: screen.width / 2,
+      alignItems: 'center',
+      justifyContent: 'center'
   },
-  buttonText:{
-    fontSize:45,
-    fontWeight:'bold',
-    color: '#DAF7A6',
+  buttonText: {
+      fontSize: 45,
+      color: '#B9AAFF'
+  },
+  timerText: {
+      color: '#fff',
+      fontSize: 90,
+      marginBottom: 20
+  },
+  buttonReset: {
+      marginTop: 20,
+      borderColor: "#FF851B"
+  },
+  buttonTextReset: {
+    color: "#FF851B"
   }
 });
